33a682de843558d706fa15066c7cb9343c2dc856
AchrafMaghous <muteallfocus7@gmail.com>
2023-03-07 18:53:20 +0100

    cpp09

--- cpp_module09/ex00/BitcoinExchange.hpp
+++ cpp_module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 16:28:46 by acmaghou          #+#    #+#             */
-/*   Updated: 2023/03/05 15:24:06 by acmaghou         ###   ########.fr       */
+/*   Updated: 2023/03/07 18:53:12 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,38 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <iostream>
-#include <map>
+#include <vector>
 #include <fstream>
 #include <sstream>
 
-class	BitcoinExchange {
-	private:
-		std::string	file;
-		bool	check_is_num(std::string str);
-	public:
-		BitcoinExchange();
-		BitcoinExchange(const std::string& str);
-		BitcoinExchange(const BitcoinExchange& obj);
-		BitcoinExchange&	operator= (const BitcoinExchange& obj);
-		~BitcoinExchange();
+// we cannot use a map here cause there could be an overlap of map keys in same date case
+// in the case of data.csv file, it is possible to use a map because of the uniqueness of dates.
 
-		std::string	getFile();
-		bool	checkDate(std::string line);
-		void	getValue(std::string line);
-		void	checkSyntax(std::string file);
+struct BitcoinExchange
+{
+	std::string	date;
+	float		value;
+
+	BitcoinExchange( std::string &date, float value ) : date(  date ), value( value ) {};
 };
+
+struct InputValues
+{
+	std::string	date;
+	float		value;
+	float		exchangedValue;
+
+	bool		tooLarge;
+	bool		notPositive;
+	bool		badInput;
+
+	InputValues(std::string &date, float value, bool tooLarge, bool notPositive, bool badInput) : date(date), value(value), exchangedValue( 0 ), tooLarge( tooLarge ), notPositive( notPositive ), badInput( badInput ) {};
+};
+
+void	btcDb(std::vector<BitcoinExchange> &bitcoinData, std::string fileName );
+void	inputFile(std::vector<InputValues> &inputData, std::string fileName );
+void	convert(const std::vector<BitcoinExchange> bitcoinData, std::vector<InputValues> &inputData );
+void	bitcoinExchange(std::string csvFileName, std::string txtFileName );
+void	printConversion(const std::vector<InputValues> inputData );
+bool	checkDate(const std::string &date );
 #endif
--- cpp_module09/ex00/main.cpp
+++ cpp_module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 16:30:37 by acmaghou          #+#    #+#             */
-/*   Updated: 2023/03/06 15:00:21 by acmaghou         ###   ########.fr       */
+/*   Updated: 2023/03/07 17:24:32 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int ac, char **av) {
 		std::cerr << "Usage: ./btc	<txt_file>\n";
 		return 1;
 	}
-	BitcoinExchange	data("data.csv");
-	data.checkSyntax(av[1]);
+	
+	bitcoinExchange("data.csv", av[1]);
 	return (0);
 }
